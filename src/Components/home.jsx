7b17619df9e606af79b74aae309e158a1c245201
Akramdadkhan <97161831+Akramdadkhan@users.noreply.github.com>
2022-03-17 12:28:32 +0530

    Home JS added

---
+++ src/Components/home.jsx
@@ -0,0 +1,17 @@
+import { Emplist } from "./EmpList";
+import {Row,Col} from "react-bootstrap";
+import "./Home.css"
+import "./Nav.css"
+
+
+export const Home = ()=>{
+    return(
+<div className="home">
+    <h1>Home</h1>
+    <p>This is Home</p>
+    
+
+
+</div>
+    )
+}
