82cf9bbf57f4388cde89d1dbda854701f5b4e94e
Akramdadkhan <97161831+Akramdadkhan@users.noreply.github.com>
2022-03-18 12:20:43 +0530

    Nav Js

---
+++ src/Components/Nav.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export const Nav=()=>{
+    return(
+        <div className="nav">
+            <a href="">Home</a>
+            <a href="">About</a>
+            <a href="">Services</a>
+            <a href="">ContactUS</a>
+        </div>
+    )
+}
